978c9c00d1ba08594471ce8973ec638fb7cd7bfa
Michael-1898 <85533503+Michael-1898@users.noreply.github.com>
2023-04-10 23:02:14 -0400

    new scenes and tutorial done
    -added two new hub scenes
    -temporarily finished tutorial/intro scene

---
+++ CCGame2023/Assets/Scripts/TransitionZone.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionZone : MonoBehaviour
+{
+    [SerializeField] int nextScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col) {
+        if(col.gameObject.CompareTag("Player")) {
+            SceneManager.LoadScene(nextScene);
+        }
+    }
+}
